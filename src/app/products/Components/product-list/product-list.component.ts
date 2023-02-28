ffe8c7e865eaf809ce1a39144ea34546b8d4509f
AbdallahAhmed0 <abdallahahmadadel@gmail.com>
2023-02-28 21:12:58 +0200

    update product list

--- src/app/products/Components/product-list/product-list.component.ts
+++ src/app/products/Components/product-list/product-list.component.ts
@@ -15,17 +15,18 @@ export class ProductListComponent implements OnInit {
   ngOnInit(): void {
     this.productService.getAllproducts().subscribe(prds => {
       this.products=prds;
+      console.log(this.products)
     })
-    this.products=[{name:"Modern Rubber Chair",code:"mb",fields:{price:95,img:"../../../../assets/img/sidebar-4.jpg",place:'Giza'}},
-    {name:"Rustic Rubber Gloves",code:"bk",fields:{price:120,img:"../../../../assets/img/sidebar-3.jpg",place:'Cairo',pages:125}},
-    {name:"Practical Concrete Computer",code:"md",fields:{price:95,images:"../../../../assets/img/sidebar-5.jpg",place:'Giza'}},
-    {name:"Modern Rubber Chair",code:"mb",fields:{price:95,images:"../../../../assets/img/sidebar-2.jpg",place:'Giza'}},
-    {name:"Modern Rubber Chair",code:"bk",fields:{price:120,img:"../../../../assets/img/sidebar-1.jpg",place:'Cairo',pages:125}},
-    {name:"Modern Rubber Chair",code:"md",fields:{price:95,images:"../../../../assets/img/mask.png",place:'Giza'}},
-    {name:"Practical Concrete Computer",code:"mb",fields:{price:95,images:"../../../../assets/img/new_logo.png",place:'Giza'}},
-    {name:"Modern Rubber Chair",code:"bk",fields:{price:120,images:"../../../../assets/img/default-avatar.png",place:'Cairo',pages:125}},
-    {name:"Modern Rubber Chair",code:"md",fields:{price:95,images:"../../../../assets/img/angular-red.png",place:'Giza'}},
-]
+//     this.products=[{name:"Modern Rubber Chair",code:"mb",fields:{price:95,img:"../../../../assets/img/sidebar-4.jpg",place:'Giza'}},
+//     {name:"Rustic Rubber Gloves",code:"bk",fields:{price:120,img:"../../../../assets/img/sidebar-3.jpg",place:'Cairo',pages:125}},
+//     {name:"Practical Concrete Computer",code:"md",fields:{price:95,images:"../../../../assets/img/sidebar-5.jpg",place:'Giza'}},
+//     {name:"Modern Rubber Chair",code:"mb",fields:{price:95,images:"../../../../assets/img/sidebar-2.jpg",place:'Giza'}},
+//     {name:"Modern Rubber Chair",code:"bk",fields:{price:120,img:"../../../../assets/img/sidebar-1.jpg",place:'Cairo',pages:125}},
+//     {name:"Modern Rubber Chair",code:"md",fields:{price:95,images:"../../../../assets/img/mask.png",place:'Giza'}},
+//     {name:"Practical Concrete Computer",code:"mb",fields:{price:95,images:"../../../../assets/img/new_logo.png",place:'Giza'}},
+//     {name:"Modern Rubber Chair",code:"bk",fields:{price:120,images:"../../../../assets/img/default-avatar.png",place:'Cairo',pages:125}},
+//     {name:"Modern Rubber Chair",code:"md",fields:{price:95,images:"../../../../assets/img/angular-red.png",place:'Giza'}},
+// ]
   }
 
 }
